2056cbca2bd2dada4217e8b162b8074a7aced275
pnhung177 <pnhung177@gmail.com>
2023-11-07 13:51:05 +0700

    Refactor the examples

---
+++ examples/Remote-Control-Tester/Remote-Control-Tester.ino
@@ -0,0 +1,33 @@
+#include <Pokerobo_RCB_master.h>
+
+const uint64_t address = 0x18580901LL;
+
+DisplayHandler displayHandler;
+RF24Tranceiver tranceiver;
+JoystickHandler joystickHandler(&tranceiver, &displayHandler);
+ProgramSelector programSelector;
+SpeedResolver speedResolver;
+
+int count = 0;
+
+void setup() {
+  Serial.begin(57600);
+
+  displayHandler.begin();
+
+  tranceiver.set(&displayHandler);
+  tranceiver.set(&speedResolver);
+  tranceiver.begin(TX, address);
+
+  joystickHandler.set(&speedResolver);
+  joystickHandler.begin();
+
+  programSelector.set(&joystickHandler);
+  programSelector.set(&tranceiver, address);
+  programSelector.set(&displayHandler);
+  programSelector.begin(PROGRAM_MODE_TEST_STATION);
+}
+
+void loop() {
+  programSelector.check();
+}
