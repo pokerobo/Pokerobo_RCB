b05ada3957d4789cd68ebfcb9820f3c76db67321
pnhung177 <pnhung177@gmail.com>
2023-11-10 11:05:01 +0700

    Fix the bug: class TwoWire' has no member named 'setWireTimeout' in ESP32

--- src/Display_Handler.cpp
+++ src/Display_Handler.cpp
@@ -41,7 +41,10 @@ U8G2_ST7567_ENH_DG128064I_1_HW_I2C u8g2(U8G2_R2, SCL, SDA, U8X8_PIN_NONE);
 
 int DisplayHandler::begin() {
   Wire.begin();
+#if defined(WIRE_HAS_TIMEOUT)
+  // Wire.setWireTimeout(timeout, reset_on_timeout)
   Wire.setWireTimeout(3000, true);
+#endif
   u8g2.setI2CAddress(0x3F * 2); 
   u8g2.setBusClock(200000);
   u8g2.begin();
