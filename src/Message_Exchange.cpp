002e15b6786fcd716ac40dcd73dddd34f6fc0725
pnhung177 <pnhung177@gmail.com>
2023-11-14 13:13:57 +0700

    Add the length() and serialize() methods to SpeedPacket

--- src/Message_Exchange.cpp
+++ src/Message_Exchange.cpp
@@ -100,6 +100,46 @@ byte SpeedPacket::getRightDirection() {
   return _RightDirection;
 }
 
+const uint8_t SpeedPacket::messageSize = sizeof(uint8_t) +
+    sizeof(uint8_t) +
+    sizeof(uint8_t);
+
+uint8_t SpeedPacket::length() {
+  return messageSize;
+}
+
+uint8_t* SpeedPacket::serialize(uint8_t* buf, uint8_t len) {
+  if (len < messageSize) {
+    return NULL;
+  }
+
+  uint8_t directionFlags = 0;
+
+  switch(_LeftDirection) {
+    case 1:
+      directionFlags |= 0b0001;
+      break;
+    case 2:
+      directionFlags |= 0b0010;
+      break;
+  }
+
+  switch(_RightDirection) {
+    case 1:
+      directionFlags |= 0b0100;
+      break;
+    case 2:
+      directionFlags |= 0b1000;
+      break;
+  }
+
+  buf[0] = directionFlags;
+  buf[1] = _LeftSpeed;
+  buf[2] = _RightSpeed;
+
+  return buf;
+}
+
 void ConsoleMessageRenderer::clear() {}
 
 void ConsoleMessageRenderer::splash(char* title, byte align) {}
