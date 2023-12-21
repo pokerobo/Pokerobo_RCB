41d269ee8afa6ae30cb678db89ff678a8632121d
pnhung177 <pnhung177@gmail.com>
2023-12-21 10:35:37 +0700

    Moves source code from the Moving_Command into Moving_Resolver

--- src/Moving_Resolver.h
+++ src/Moving_Resolver.h
@@ -7,7 +7,27 @@
 #include "Message_Exchange.h"
 #include "Message_Serializer.h"
 #include "Message_Renderer.h"
-#include "Moving_Command.h"
+
+#define MOVING_COMMAND_WEIGHT_MAX             255
+
+class MovingCommand: public MessageInterface {
+  public:
+    static const uint8_t messageSize;
+    MovingCommand(int leftSpeed=0, byte leftDirection=0, int rightSpeed=0, byte rightDirection=0);
+    void update(int leftSpeed, byte leftDirection, int rightSpeed, byte rightDirection);
+    int getLeftSpeed();
+    byte getLeftDirection();
+    int getRightSpeed();
+    byte getRightDirection();
+    uint8_t length();
+    uint8_t* serialize(uint8_t* buf, uint8_t len);
+    MessageInterface* deserialize(uint8_t* buf);
+  private:
+    int _LeftSpeed;
+    byte _LeftDirection;
+    int _RightSpeed;
+    byte _RightDirection;
+};
 
 class MovingCommandResolver {
   public:
