d665a9b51e72e027d690dfafd2323a9bf3a819c7
pnhung177 <pnhung177@gmail.com>
2024-07-12 19:11:28 +0700

    Refactor the TransmissionCounter class

--- src/Message_Monitor.cpp
+++ src/Message_Monitor.cpp
@@ -16,6 +16,49 @@ uint32_t TransmissionCounter::getPacketLossTotal() {
   return packetLossTotal;
 }
 
+void TransmissionCounter::adjust() {
+  if (this->ordinalNumber >= 999999UL) {
+    this->reset();
+  }
+}
+
+void TransmissionCounter::reset() {
+  this->baselineNumber = 0;
+  this->ordinalNumber = 0;
+  this->continualLossCount = 0;
+  this->packetLossTotal = 0;
+}
+
+void TransmissionCounter::beginTransmission() {
+  this->ordinalNumber += 1;
+  this->packetLossTotal += 1;
+}
+
+void TransmissionCounter::confirmTransmissionSuccess() {
+  this->continualLossCount = 0;
+  this->packetLossTotal -= 1;
+}
+
+void TransmissionCounter::confirmTransmissionFailure() {
+  this->continualLossCount += 1;
+}
+
+void TransmissionCounter::update(uint32_t count) {
+  if (this->ordinalNumber == 0) {
+    this->baselineNumber = count;
+    this->packetLossTotal = 0;
+  } else {
+    if (count < this->ordinalNumber + 1) {
+      this->baselineNumber = count;
+      this->packetLossTotal = 0;
+    } else if (count == this->ordinalNumber + 1) {
+    } else if (count > this->ordinalNumber + 1) {
+      this->packetLossTotal += count - this->ordinalNumber - 1;
+    }
+  }
+  this->ordinalNumber = count;
+}
+
 bool TransmissionMonitor::isCounterBuiltin() {
   return _counterBuiltin;
 }
--- src/Message_Monitor.h
+++ src/Message_Monitor.h
@@ -9,49 +9,18 @@ typedef struct _TransmissionCounter {
     uint32_t ordinalNumber = 0;
     uint32_t continualLossCount = 0;
     uint32_t packetLossTotal = 0;
+
   public:
     uint32_t getBaselineNumber();
     uint32_t getOrdinalNumber();
     uint32_t getContinualLossCount();
     uint32_t getPacketLossTotal();
-
-    void adjust() {
-      if (this->ordinalNumber >= 999999UL) {
-        this->reset();
-      }
-    }
-    void reset() {
-      this->baselineNumber = 0;
-      this->ordinalNumber = 0;
-      this->continualLossCount = 0;
-      this->packetLossTotal = 0;
-    }
-    void beginTransmission() {
-      this->ordinalNumber += 1;
-      this->packetLossTotal += 1;
-    }
-    void confirmTransmissionSuccess() {
-      this->continualLossCount = 0;
-      this->packetLossTotal -= 1;
-    }
-    void confirmTransmissionFailure() {
-      this->continualLossCount += 1;
-    }
-    void update(uint32_t count) {
-      if (this->ordinalNumber == 0) {
-        this->baselineNumber = count;
-        this->packetLossTotal = 0;
-      } else {
-        if (count < this->ordinalNumber + 1) {
-          this->baselineNumber = count;
-          this->packetLossTotal = 0;
-        } else if (count == this->ordinalNumber + 1) {
-        } else if (count > this->ordinalNumber + 1) {
-          this->packetLossTotal += count - this->ordinalNumber - 1;
-        }
-      }
-      this->ordinalNumber = count;
-    }
+    void adjust();
+    void reset();
+    void beginTransmission();
+    void confirmTransmissionSuccess();
+    void confirmTransmissionFailure();
+    void update(uint32_t count);
 } TransmissionCounter;
 
 class TransmissionMonitor {
